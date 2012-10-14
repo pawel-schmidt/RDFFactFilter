de6d61e15fa92a25fe79d1efe930968ea50739a6
Paweł Schmidt <paw3l.schmidt@gmail.com>
2012-10-14 15:59:59 +0200

    Triple type added
    Add: class Triple for representing RDF triple.

---
+++ RDFRepresentation/Triple.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDFRepresentation {
+    public class Triple {
+        private String @object;
+        private String subject;
+        private String property;
+
+        public Triple(String @object, String property, String subject) {
+            this.@object = @object;
+            this.property = property;
+            this.subject = subject;
+        }
+
+        public String Object {
+            get { return @object; }
+            set { @object = value; }
+        }
+
+        public String Subject {
+            get { return subject; }
+            set { subject = value; }
+        }
+
+        public String Property {
+            get { return property; }
+            set { property = value; }
+        }
+
+    }
+}
