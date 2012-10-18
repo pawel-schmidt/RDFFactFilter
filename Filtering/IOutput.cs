dcfe3d7d5b5236e9555da43b09e32acc9fd59b13
Paweł Schmidt <paw3l.schmidt@gmail.com>
2012-10-18 15:13:18 +0200

    Output interface
    Add: IOutput interface to save triples.

---
+++ Filtering/IOutput.cs
@@ -0,0 +1,18 @@
+﻿using RDFRepresentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filtering {
+    public interface IOutput {
+
+        /// <summary>
+        /// Save triples to file.
+        /// </summary>
+        /// <param name="triples">Filtered triples to save.</param>
+        /// <param name="filePath">Path to file for save.</param>
+        public void save(List<Triple> triples, String filePath);
+    }
+}
