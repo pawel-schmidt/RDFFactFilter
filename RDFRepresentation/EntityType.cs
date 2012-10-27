be369d7a8018f97c5d3524714f5a47b50d843122
Paweł Schmidt <paw3l.schmidt@gmail.com>
2012-10-27 11:58:29 +0200

    EntityType expansion
    A: method 'getHierarchyString' to get hierarchy string;
    A: method 'getType' to find node which value is passed as parameter. If
    hierarchy contains more than one node with value equal to parameter,
    method returns nearest common node.

--- RDFRepresentation/EntityType.cs
+++ RDFRepresentation/EntityType.cs
@@ -22,13 +22,58 @@ namespace RDFRepresentation {
         }
         
         [JsonConstructor]
-        public EntityType(String name, List<EntityType>  subtypes) {
+        public EntityType(String name, List<EntityType> subtypes) {
             this.name = name;
             this.subtypes = subtypes;
         }
         
         override public String ToString() {
             return name;
+        }
+
+        public String getHierarchyString() {
+            return subtypesToString(this, 0);
+        }
+
+        private String subtypesToString(EntityType types, int level) {
+            StringBuilder result = new StringBuilder();
+            for (int i = 1; i < level; ++i) {
+                result.Append("|   ");
+            }
+            if (level > 0) result.Append("|-");
+            result.Append(types.name);
+            result.Append("\n");
+            foreach (EntityType et in types.subtypes) {
+                result.Append(subtypesToString(et, level + 1));
+            }
+            return result.ToString();
+        }
+
+        public EntityType getType(String typeToFind) {
+            EntityType result = search(this, typeToFind);
+            return result == null? this : result;
+        }
+
+        private static EntityType search(EntityType type, String typeToFind) {
+            if (type.name.Equals(typeToFind, StringComparison.OrdinalIgnoreCase)) {
+                return type;
+            }
+            EntityType result = null;
+            EntityType secondResult = null;
+            foreach (EntityType et in type.subtypes) {
+                if (result == null)
+                    result = search(et, typeToFind);
+                else if (secondResult == null)
+                    secondResult = search(et, typeToFind);
+                else
+                    return type;
+            }
+            if (secondResult != null)
+                return type;
+            if (result != null)
+                if (type.subtypes.Contains(result) && result.name.Equals(typeToFind, StringComparison.OrdinalIgnoreCase))
+                    return type;
+            return result;
         }
     }
 }
